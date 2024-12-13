5106cc9b59eeae43edfcd8ac53c49b4ae30f4d57
KikyoNanakusa <nanakusa.vrc@gmail.com>
2024-12-14 02:12:00 +0900

    chore: Update error messages

--- llm4decompile/evaluate.py
+++ llm4decompile/evaluate.py
@@ -337,7 +337,7 @@ def process_row(row, args):
         asm_all = compile_and_write(row["fname"], c_source_code)
         # Check compile was successful for all optimization
         if len(asm_all) != 4:
-            print(f"{row['fname']} is not compilable")
+            logger.error(f"{row['fname']} is not compilable")
             return None
 
         prompts = []
